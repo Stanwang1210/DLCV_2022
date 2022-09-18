1794a88d44e6bc2c80ed35a0d4c5b6141d70748e
Stanwang <ch995308@gmail.com>
2022-09-18 21:20:11 +0800

    tune param

--- HW1_stan/model_p2.py
+++ HW1_stan/model_p2.py
@@ -3,18 +3,18 @@ from torch import nn
 import torch.nn.functional as F
 class Model(nn.Module):
 
-    def __init__(self, model, fcn_model) -> None:
+    def __init__(self, model) -> None:
         super().__init__()
         self.feature_extractor = model.features
         self.fcn = nn.Sequential(
-            nn.Conv2d(in_channels=512, out_channels=128, kernel_size = (2, 2)),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size = (1, 1)),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=7, kernel_size = (1, 1)),
-            # nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size = (1, 1)),
+            nn.ReLU(),
             # nn.Conv2d(in_channels=32, out_channels=7, kernel_size = (1, 1)),
            
             # nn.Upsample(size=(128, 128), mode='bilinear'),
-            nn.Upsample(size=(512, 512), mode='bilinear'),
+            nn.ConvTranspose2d(in_channels=256, out_channels=7, kernel_size=32, stride=32, dilation=1)  # [in_channel, out_channel, kernel_size, stride]
             # nn.Upsample(scale_factor=2, mode='bilinear'),
 
         )
--- HW1_stan/p2_train.py
+++ HW1_stan/p2_train.py
@@ -31,16 +31,15 @@ def main(args):
     fix(args.seed)
     transform = transforms.Compose([
         # transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(),
-        # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
+        # transforms.RandomHorizontalFlip(),
+        # transforms.ColorJitter(brightness=0.1, contrast=1, saturation=0.1, hue=0.1),
         # transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15, resample=False, fillcolor=0),
         transforms.ToTensor(),
         # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ])
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.hub.load('pytorch/vision:v0.10.0', args.model_name_or_path, pretrained=True)
-    fcn_model = torch.hub.load('pytorch/vision:v0.10.0', 'fcn_resnet50', pretrained=True)
-    model = Model(model, fcn_model)
+    model = Model(model)
     train_dataset = P2_Dataset(data_dir=args.data_dir, task='train', transform=transform)
     valid_dataset = P2_Dataset(data_dir=args.data_dir, task='validation', transform=transform)
     train_loader = DataLoader(
@@ -55,7 +54,8 @@ def main(args):
         shuffle=False,
         num_workers=0,
     )
-    criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.1, 0.3, 0.1, 0.2, 0.1, 0.2]).to(device))
+    # criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.1, , 0.1, 0.2, 0.1, 0.2]).to(device))
+    criterion = nn.CrossEntropyLoss()
     optimizer = getattr(torch.optim, args.optimizer)(
         model.parameters(),
         lr=args.lr,
@@ -114,48 +114,48 @@ def main(args):
             Eval_IOU.append(np.mean(IOU)*100)
             for i in range(len(Total_iou)):
                 Eval_total_IOU[i].append(np.mean(Total_iou[i])*100)
-    plt.plot(Train_Loss, label='train loss')
-    plt.plot(Eval_Loss, label='eval loss')
-    plt.legend(loc='best')
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.title('P2 Loss')
-    plt.savefig("P2_Loss.jpg")
-    plt.clf()
-    plt.plot(Train_IOU, label='train iou')
-    for i in range(len(Train_total_IOU)):
-        plt.plot(Train_total_IOU[i], label=f'IOU_{i}')
-    plt.legend(loc='best')
-    plt.xlabel('epoch')
-    plt.ylabel('iou')
-    plt.title('P2 Train IOU')
-    plt.savefig("P2_train_IOU.jpg")
-    plt.clf()
-    plt.plot(Eval_IOU, label='eval iou')
-    for i in range(len(Eval_total_IOU)):
-        plt.plot(Eval_total_IOU[i], label=f'IOU_{i}')
-    plt.legend(loc='best')
-    plt.xlabel('epoch')
-    plt.ylabel('iou')
-    plt.title('P2 IOU')
-    plt.savefig("P2_eval_IOU.jpg")
-    plt.clf()
+        plt.plot(Train_Loss, label='train loss')
+        plt.plot(Eval_Loss, label='eval loss')
+        plt.legend(loc='best')
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.title('P2 Loss')
+        plt.savefig("P2_Loss.jpg")
+        plt.clf()
+        plt.plot(Train_IOU, label='train iou')
+        for i in range(len(Train_total_IOU)):
+            plt.plot(Train_total_IOU[i], label=f'IOU_{i}')
+        plt.legend(loc='best')
+        plt.xlabel('epoch')
+        plt.ylabel('iou')
+        plt.title('P2 Train IOU')
+        plt.savefig("P2_train_IOU.jpg")
+        plt.clf()
+        plt.plot(Eval_IOU, label='eval iou')
+        for i in range(len(Eval_total_IOU)):
+            plt.plot(Eval_total_IOU[i], label=f'IOU_{i}')
+        plt.legend(loc='best')
+        plt.xlabel('epoch')
+        plt.ylabel('iou')
+        plt.title('P2 IOU')
+        plt.savefig("P2_eval_IOU.jpg")
+        plt.clf()
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     # randomness
     parser.add_argument("--seed", type=int, default=9999)
     # model 
-    parser.add_argument("--model_name_or_path", type=str, default='vgg19')
+    parser.add_argument("--model_name_or_path", type=str, default='vgg16')
 
     # data
     parser.add_argument("--data_dir", type=str, default='hw1_data/p2_data')
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=32)
     # hyperparams
     parser.add_argument("--optimizer", type=str, default="Adam")
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--weight_decay", type=float, default=1e-5)
-    parser.add_argument("--epoch", type=int, default=3)
+    parser.add_argument("--lr", type=float, default=2e-4)
+    parser.add_argument("--weight_decay", type=float, default=1e-3)
+    parser.add_argument("--epoch", type=int, default=50)
     parser.add_argument("--step_size", type=int, default=200)
     parser.add_argument("--gamma", type=float, default=0.5)
 
